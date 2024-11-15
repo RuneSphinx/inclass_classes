555be45fb0b09d66bf04975501d730ab1d4f9ad4
RuneSphinx <30443828+RuneSphinx@users.noreply.github.com>
2024-11-14 18:03:50 -0700

    lots of changes, created new student class file and main file

--- student.py
+++ student.py
@@ -46,24 +46,3 @@ class Student:
 
     def change_grade(self, grade):
         self.grade = str(grade)
-
-#jay_pike is an instance of the student class
-jay_pike = Student.from_full_name("Jay Pike", "Sophomore", 3456) #Object of the student class
-jane_doe = Student("Jane", "Doe", "Senior", 1234) #object of the student class
-waldo_wildcat = Student("Waldo", "Wildcat", "Senior")
-
-
-# print(jay_pike.email)
-# print(jane_doe.email)
-# waldo_wildcat.print_student_data()
-# waldo_wildcat.change_grade("Freshman")
-# waldo_wildcat.print_student_data()
-
-# print("Printing out object")
-# print(jay_pike)
-# print(jane_doe)
-# print(waldo_wildcat)
-
-all_students_in_class = Student.get_all_students()
-for student in all_students_in_class:
-    student.print_student_data()
