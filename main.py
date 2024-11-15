6343f1a227fc5af1c0e8cc98505b63b84d44fcd1
RuneSphinx <30443828+RuneSphinx@users.noreply.github.com>
2024-11-14 18:32:22 -0700

    create a child class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from student import Student
+from graduate_student import Graduate_Student
 
 #jay_pike is an instance of the student class
 jay_pike = Student.from_full_name("Jay Pike", "Sophomore", 3456) #Object of the student class
 jane_doe = Student("Jane", "Doe", "Senior", 1234) #object of the student class
 waldo_wildcat = Student("Waldo", "Wildcat", "Senior")
+grad_student_1 = Graduate_Student("Michael", "Scott", "Graduate")
 
 
 # print(jay_pike.email)
